e61a4cdff22dfe3f2b8556ee2e614c48fe4e129e
Anatolii Perfun <tolikvik@gmail.com>
2022-02-05 02:24:35 +0200

    new commit

--- proj2/script.js
+++ proj2/script.js
@@ -1,33 +1,49 @@
-
-const qouteContainer = document.getElementById('quote-container');
+const quoteContainer = document.getElementById('quote-container');
 const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
-const twitterBtn = document.getElementById('twitter');
-const newQuoteBtn = document.getElementById('new-qoute');
+const twitterBtn= document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
 
 let apiQuotes = [];
 
-//show new quotes
-function newQuotes() {
-   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-   console.log(quote);
+// Set quote
+quoteText.textContent = quoteText;
+
+//New Quote
+function newQuote(){
+    //Random quote
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    
+    quoteText.textContent = quote.text;
+    authorText.textContent = quote.author ? quote.author : "Unknown";
 }
 
-// get quotes from API 
-async function getQuotes() {
-   const apiUrl = 'https://type.fit/api/quotes';
-   try {
-      const response = await fetch(apiUrl);
-      apiQuotes = await response.json();
-      newQuotes();
-   } catch (error) {
-      alert('Error: ' + error.message);
-   }
+//Get Quotes fom API
+async function getQuotes(){
+
+    const apiUrl = `https://type.fit/api/quotes`;
+    try {
+        const response = await fetch(apiUrl);
+        apiQuotes = await response.json();
+        newQuote();
+    } catch (error){
+      error.message = "Error fetchingQuotes: " + error.message;
+    }
 }
 
-//on loading
+function tweetQuote() {
+    const twitterUrl =`https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl, '_blank');
+}
+
+//Event Listener 
+if (newQuoteBtn) {
+   newQuoteBtn.addEventListener('click', newQuote);
+}
+if (twitterBtn) {
+   twitterBtn.addEventListener('click', tweetQuote);
+}
+//On load
 getQuotes();
-// newQuote();
-
-
