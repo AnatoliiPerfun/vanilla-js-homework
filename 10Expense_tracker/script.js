8df3493eeffa48c440fc409f16a390dedf49fe53
Anatolii Perfun <tolikvik@gmail.com>
2022-02-14 00:20:09 +0200

    added js

--- 10Expense_tracker/script.js
+++ 10Expense_tracker/script.js
@@ -5,3 +5,86 @@ const list = document.getElementById('list');
 const form = document.getElementById('form');
 const text = document.getElementById('text');
 const amount = document.getElementById('amount');
+
+
+
+const localStorageTransactions = JSON.parse(localStorage.getItem('transactions'));
+let transactions = localStorage.getItem('transactions') !== null ? localStorageTransactions : [];
+ 
+// Add transactions
+function addTransaction(e) {
+   e.preventDefault();
+
+   if(text.value.trim() === '' || amount.value.trim() === '') {
+      alert('Please enter text and amount');
+   } else {
+      const transaction = {
+         id: generateID(),
+         text: text.value,
+         amount: +amount.value
+      };
+
+      transactions.push(transaction);
+      addTransactionDOM(transaction);
+
+      updateValues();
+      updateLocalStorage();
+      text.value = '';
+      amount.value = '';
+   }
+}
+
+// Generate random id
+function generateID() {
+   return Math.floor(Math.random() * 100000);
+}
+
+function addTransactionDOM(transaction) {
+   const sign = transaction.amount < 0 ? '-' : '+';
+   const item = document.createElement('li');
+   item.classList.add(transaction.amount < 0 ? 'minus' : 'plus');
+   item.innerHTML = `
+   ${transaction.text} <span>${sign}${Math.abs(transaction.amount)}</span>
+   <button class="delete-btn" onclick="removeTransaction(${transaction.id})">X</button>
+   `;
+   list.appendChild(item);
+}
+
+// Update cash balance
+function updateValues() {
+   const amounts = transactions.map(transaction => transaction.amount);
+   const total = amounts
+      .reduce((acc, item) => (acc += item), 0).toFixed(2);
+   const income = amounts
+      .filter(item => item > 0)
+      .reduce((acc, item) => (acc += item), 0).toFixed(2);
+   const expense = (amounts.filter(item => item < 0)
+      .reduce((acc, item) => (acc += item), 0) * -1).toFixed(2);
+   
+   balance.innerText = `$${total}`;
+   money_plus.innerText = `$${income}`;
+   money_minus.innerText = `$${expense}`;
+}
+
+// Remove transaction by ID
+function removeTransaction(id) {
+   transactions = transactions.filter(transaction => transaction.id !== id);
+   updateLocalStorage();
+   init();
+}
+
+// Update local storage transactions
+function updateLocalStorage() {
+   localStorage.setItem('transactions', JSON.stringify(transactions));
+}
+
+// Init app
+function init() {
+   list.innerHTML = '';
+   transactions.forEach(addTransactionDOM);
+   updateValues();
+}
+
+init();
+
+form.addEventListener('submit', addTransaction);
